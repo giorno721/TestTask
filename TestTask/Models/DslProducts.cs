4c1b1538c9c380b7463a1198057b6c4ecbc63994
Roman Marushko <roman142480@gmail.com>
2025-06-04 19:12:47 +0300

    add models

---
+++ TestTask/Models/DslProducts.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTask.Models;
+
+public class DslProducts
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public string Product { get; set; }
+}
---
+++ TestTask/Models/TvProducts.cs
@@ -0,0 +1,8 @@
+﻿namespace TestTask.Models;
+
+public class TvProducts
+{
+    public int Id { get; set; }
+    public int CustomerId { get; set; }
+    public string Product { get; set; }
+}
